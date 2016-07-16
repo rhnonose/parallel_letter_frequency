2100f4d82898015d101b900a837e7f6f21fc8c01
rhnonose <rodrigo.nonose89@gmail.com>
2016-07-16 20:26:32 -0300

    Spawn workers to count letter frequency.

--- lib/frequency.ex
+++ lib/frequency.ex
@@ -7,30 +7,59 @@ defmodule Frequency do
 
   The number of worker processes to use can be set with 'workers'.
   """
+  def frequency([], _) do
+  	%{}
+  end
+
   @spec frequency([String.t], pos_integer) :: map
-  def frequency(texts, _workers) do
+  def frequency(texts, workers) do
   	texts
   	|> Enum.join
   	|> filter_letters
+  	|> count_frequency(workers)
   end
+
 
   def filter_letters(text) do
   	text
   	|> String.downcase
   	|> String.codepoints
   	|> Enum.filter(fn letter -> !Enum.member?(
-  		[" ", ",", ".", "?", "!", "'"],
+  		[" ", ",", ".", "?", "!", "'", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
         letter) end)
+  end
+
+  def count_frequency(text, workers) do
+  	1..workers
+  	|> Enum.map(fn _ -> spawn(__MODULE__, :count_letter, [self]) end)
+  	|> schedule_processes(text, Enum.uniq(text), %{})
+  end
+
+  def schedule_processes(processes, text, letters, result_map) do
+  	receive do
+  		{:ready, pid} when length(letters) > 0 ->
+  			[h | t] = letters
+  			send pid, {:count, text, h, self}
+  			schedule_processes(processes, text, t, result_map)
+  		{:ready, pid} ->
+  			send pid, {:end}
+  			if length(processes) > 1 do
+  				rest = List.delete(processes, pid)
+  				schedule_processes(rest, text, letters, result_map)
+  			else
+  				result_map	
+  			end
+  		{:freq, count, letter} ->
+  			schedule_processes(processes, text, letters, Enum.into(result_map, %{letter => count}))
+  	end
   end
 
   def count_letter(squeduler) do
   	send squeduler, {:ready, self}
   	receive do
   		{:count, text, letter, resp} ->
-  			count = text
-  			|> String.codepoints
-  			|> Enum.count(fn n -> n == letter end)
-  			send resp, {:freq, count}
+  			send resp, {:freq, Enum.count(text, fn n -> n == letter end), letter}
+  			count_letter(resp)
   		{:end} -> exit(:normal)
   	end
   end
